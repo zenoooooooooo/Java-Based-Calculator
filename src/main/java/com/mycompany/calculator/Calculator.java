ea23d940458f6f7e42022a53a6bd1f1619548702
zenoooooooooo <esplanaejhaym@gmail.com>
2024-05-12 01:34:53 +0800

    Initial Commit

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author akira
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        Form f = new Form();
+        f.show();
+            
+    }
+}
